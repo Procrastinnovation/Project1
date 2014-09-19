b5f8a0fa09b126ce291622968318ec49cfef9c61
John Lim <JohnLim@g.harvard.edu>
2014-09-18 23:41:33 -0400

    JLim 18SEP2014 Initial build

---
+++ README.md
@@ -0,0 +1,15 @@
+#Project 1
+Created by: John Lim
+
+Description:
+This is for DWA15 Fall semester's first project where HTML and CSS were used to launch the initial baseline for the projects for this class. 
+
+Demo:
+All the hyperlinks are self explanatory
+
+Details for teachning team:
+custom CSS were used to demonstrate basic CSS skills.
+Bootstrap were also used for extra challenge.
+
+Outsource:
+Credits and disclose slide images were from http://hdw.eweb4.com
---
+++ index.php
@@ -0,0 +1,117 @@
+<!--This is a comment. Comments are not displayed in the browser-->
+<!--18SEP2014 JLim Initial release for Project 1. Bootstrap were used for additional CSS effect-->
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>John Lim P1 - DWA15 Portfolio</title>
+	<meta charset="utf-8">
+	<!--<meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css">
+-->
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Theme Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap theme -->
+    <link href="dist/css/bootstrap-theme.min.css" rel="stylesheet">
+
+
+<!-- Bootstrap core CSS -->
+    <link href="dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap theme -->
+    <link href="dist/css/bootstrap-theme.min.css" rel="stylesheet">
+
+ <!-- Custom styles for this template by JLim -->
+    <link href="theme.css" rel="stylesheet">
+
+</head>
+<body>
+
+<div class="container">
+<img class="img-circle" src='images/John_Lim_HeadShot.jpg' alt="John Lim image" style="width: 140px; height: 187px;">
+<h1>John Lim <br> Dynamic Web Apps Portfolio</h1>
+
+ <!-- Carousel
+    ================================================== -->
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="item active">
+          <img src='images/projects2.jpg' alt="First slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Project 2 Title</h1>
+              <p>View: <code>[link]</code><br>Github:<code>[link]</code></p>
+              <p><a class="btn btn-lg btn-primary" href="#" role="button">Project 2</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img src='images/projects3.jpg' alt="Second slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Project 3 Title</h1>
+              <p>View: <code>[link]</code><br>Github:<code>[link]</code></p>
+              <p><a class="btn btn-lg btn-primary" href="#" role="button">Project 3</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img src='images/projects4.jpg' alt="Third slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Project 4 Title</h1>
+              <p>View: <code>[link]</code><br>Github:<code>[link]</code></p>
+              <p><a class="btn btn-lg btn-primary" href="#" role="button">Project 4</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+    </div><!-- /.carousel -->
+</div><!-- /class="container"> -->
+
+<div class="container3">
+<h2>In 2+ sentences describe who you are and what you do.</h2>
+		<p>I am currently working full time as a software developer at Almac Group which is a pharmaceutical company where we build clinical trials. We mostly use SQL server stored procedure, XML, Jscript and C# to create web and phone access clinical trial that gets approved by the FDA. Our system is accessible via phone or web and is highly configurable and customizable. IXRS empowers clients to proactively manage key aspects of their clinical trials, including randomization, dosing, drug/clinical supplies inventory management, electronic Patient Reported Outcomes (ePRO).</p>
+
+<h2>Why are you taking this course?</h2>
+		<p>With current experience with HTML/CSS and looking to take my web programming skills to the next level with server-side web development, Dynamic Web Application class seems to be a good place to start. I do have HTML/CSS and PHP experience already but, I haven’t used them for about several years. This course will count as one of my enrollment courses for Harvard Extension School. I am in a pursuit of getting master’s degree in fields of Information Technology.</p>
+		
+		<h2>What previous experience do you have with programming?</h2>
+		<p>I have a bachelor’s degree in ‘Information Sciences and Technology’ in concentration in designing and development with minor in ‘Security Risks and Analysis’. Therefore, I have experience with Visual basic, C++, C#, Java, Processing, max/MSP, Arduino, HTML, XML, JavaScript, SQL and other business applications.</p>
+		
+		<h2>Are you working on a Mac, PC, or some other platform?</h2>
+		<p>I have experience with PC, Mac, Ubuntu and other Linux operating system but, for this course, I will be using PC running on Window 7 64bit.</p>
+		
+		<h2>Anything else we should know?</h2>
+		<p>On free time, I practice Taekwondo, gymnastic and many other outdoor sports. I also play acrostic and electric guitar.</p>
+
+<!-- FOOTER -->
+      <footer>
+	<p>-Credits-</p>
+	<p>Carousel &middot; <a href='http://hdw.eweb4.com/search/1920x1200+city/'  target="_blank")">>Images</a> Extra Challenge &middot; <a href='http://getbootstrap.com/'  target="_blank")">>BootStrap</a> </p>
+        <p class="pull-right"><a href="#">Back to top</a></p>
+        <p>&copy; 2014 Fall DWA15 - John Lim</p>
+      </footer>
+
+</div><!-- /class="container2"> -->
+
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="dist/js/bootstrap.min.js"></script>
+
+</body>
+</html>
